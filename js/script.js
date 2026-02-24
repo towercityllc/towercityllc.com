dff5bdccee5a6ab9136603377039ffc79d086c78
towercityllc <infotowercityllc@gmail.com>
2026-02-24 07:03:59 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ window.addEventListener("scroll", function() {
     }
   });
 });
+// Project filter
+const filterButtons = document.querySelectorAll('.filter-btn');
+const projects = document.querySelectorAll('.project-item');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        document.querySelector('.filter-btn.active').classList.remove('active');
+        button.classList.add('active');
+
+        const filter = button.getAttribute('data-filter');
+
+        projects.forEach(project => {
+            if (filter === 'all') {
+                project.style.display = 'block';
+            } else {
+                project.style.display =
+                    project.classList.contains(filter) ? 'block' : 'none';
+            }
+        });
+    });
+});
